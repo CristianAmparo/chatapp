ba86f44845f3849ad971337d53c784e0196238c2
CristianAmparo <christianamparo132@gmail.com>
2023-09-15 15:33:53 +0800

    chatapp

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    if(isset($_SESSION['UNIQUE_ID'])){ //if user is logged in
+        header("location: users.php");
+    }
+?>
+
+<?php include_once "header.php"; ?>
+    <body>
+        
+        <div class="wrapper">
+            <section class="form login">
+                <header style="text-align: center; color: blue;">Chat App</header>
+                <form action="#">
+                    <div class="error-txt"></div>
+                    <div class="field input">
+                        <label>Email Address</label>
+                        <input type="text" name="email" placeholder="Enter your email" required="required">
+                    </div>
+                    <div class="field input">
+                        <label>Password</label>
+                        <input type="password" name="password" placeholder="Enter your password" required="required">
+                        <i class="fas fa-eye"></i>
+                    </div>
+                    <div class="field button">
+                        <input type="submit" value="Continue to chat">
+                    </div>
+                </form>
+                <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+            </section>
+        </div>
+
+        <script type="text/javascript" src="javascript/pass-show-hide.js"></script>
+        <script type="text/javascript" src="javascript/login.js"></script>
+        
+    </body>
+</html>
---
+++ php/users.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION['UNIQUE_ID'])) {
+    header("location: ../login.php");
+}
+include_once "config.php";
+$outgoing_id = $_SESSION['UNIQUE_ID'];
+if ($_SESSION['UNIQUE_ID'] != 607444104) {
+
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = 607444104 ");
+    $output = "";
+
+    if (mysqli_num_rows($sql) === 1) {
+        include "data.php";
+    }
+} else {
+    $sql = mysqli_query($conn, "SELECT users.*, MAX(messages.timestamp) AS latest_timestamp FROM users JOIN messages ON users.unique_id = messages.outgoing_msg_id WHERE NOT users.unique_id = 607444104 GROUP BY users.unique_id ORDER BY latest_timestamp DESC;");
+    $output = "";
+    if (mysqli_num_rows($sql) == 0) { //If there is only one row in the database then definitely this is a current logged in user.so we tell there is no user to chat if users is equal to 1
+        $output .= "No users are available to chat";
+    } elseif (mysqli_num_rows($sql) > 0) { //We'll show all users
+        include "data.php";
+    }
+}
+echo $output;
